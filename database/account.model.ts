dc707a5b34deb483bd954d751075143b4ab0dc00
@marina.smirnova <marina.smirnova@axenix.pro>
2025-09-06 19:17:27 +0300

    implement account model

--- database/account.model.ts
+++ database/account.model.ts
@@ -8,14 +8,17 @@ export interface IAccount {
   provider: string;
   providerAccountId: string;
 }
-const AccountSchema = new Schema({
-  userId: { type: Types.ObjectId, ref: "User", required: true },
-  name: { type: String, required: true },
-  image: { type: String },
-  password: { type: String, required: true },
-  provider: { type: String, required: true },
-  providerAccountId: { type: String, required: true },
-, {timestamps: true});
+const AccountSchema = new Schema(
+  {
+    userId: { type: Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    image: { type: String },
+    password: { type: String, required: true },
+    provider: { type: String, required: true },
+    providerAccountId: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Account = models?.account || model<IAccount>("Account", AccountSchema);
 export default Account;
