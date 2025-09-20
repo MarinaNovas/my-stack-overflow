1b58d2fb4244f884b55bb5c19c25bf630a5d9f72
Marina S. <marismirnovastar@gmail.com>
2025-09-20 19:46:36 +0300

    imlement Create & Read User Route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,10 @@
-import { User } from "lucide-react";
 import { NextResponse } from "next/server";
 
+import User from "@/database/user.model";
 import handlerError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validation";
 
 export async function GET() {
   try {
@@ -19,6 +21,20 @@ export async function POST(request: Request) {
   try {
     await dbConnect();
     const body = await request.json();
+    const validatedDate = UserSchema.safeParse(body);
+    if (!validatedDate.success) {
+      throw new ValidationError(validatedDate.error.flatten().fieldErrors);
+    }
+    const { email, username } = validatedDate.data;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) throw new Error("User already exists");
+
+    const existingUserName = await User.findOne({ username });
+    if (existingUserName) throw new Error("User already exists");
+
+    const newUser = await User.create(validatedDate.data);
+    return NextResponse.json({ success: true, data: newUser }, { status: 201 });
   } catch (error) {
     return handlerError(error, "api") as APIErrorResponse;
   }
