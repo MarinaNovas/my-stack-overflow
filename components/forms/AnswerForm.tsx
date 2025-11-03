685c3982ec4ae4b5c0a0c49db5999a2694c19899
Marina S. <marismirnovastar@gmail.com>
2025-11-03 15:42:49 +0300

    implement perfect ai-answer

--- components/forms/AnswerForm.tsx
+++ components/forms/AnswerForm.tsx
@@ -5,6 +5,7 @@ import { MDXEditorMethods } from "@mdxeditor/editor";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 import { useRef, useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -13,15 +14,23 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { createAnswer } from "@/lib/actions/answer.action";
+import { api } from "@/lib/api";
 import { AnswerSchema } from "@/lib/validation";
 
 const Editor = dynamic(() => import("@/components/editor"), {
   ssr: false,
 });
 
-const AnswerForm = ({ questionId }: { questionId: string }) => {
+interface IProps {
+  questionId: string;
+  questionTitle: string;
+  questionContent: string;
+}
+
+const AnswerForm = ({ questionId, questionTitle, questionContent }: IProps) => {
   const [isAnswering, startAnsweringTransition] = useTransition();
   const [isAISubmitting, setIsAISubmitting] = useState(false);
+  const session = useSession();
 
   const editorRef = useRef<MDXEditorMethods>(null);
 
@@ -44,12 +53,56 @@ const AnswerForm = ({ questionId }: { questionId: string }) => {
         toast.success("Success", {
           description: "Your answer has been posted successfully",
         });
+        if (editorRef.current) {
+          editorRef.current.setMarkdown("");
+        }
       } else {
         toast.error(`Error ${result?.status}`, {
           description: result?.error?.message,
         });
       }
     });
+  };
+
+  const generateAIAnswer = async () => {
+    if (session.status !== "authenticated") {
+      return toast.success("Success", {
+        description: "Please log in. You need to be logged in to use this feature",
+      });
+    }
+
+    setIsAISubmitting(true);
+
+    const userAnswer = editorRef.current?.getMarkdown();
+
+    try {
+      const { success, data, error } = await api.ai.getAnswer(questionTitle, questionContent, userAnswer);
+
+      if (!success) {
+        return toast.error(`Error`, {
+          description: error?.message,
+        });
+      }
+
+      const formattedAnswer = data.replace(/<br>/g, " ").toString().trim();
+
+      if (editorRef.current) {
+        editorRef.current.setMarkdown(formattedAnswer);
+
+        form.setValue("content", formattedAnswer);
+        form.trigger("content");
+      }
+
+      toast.success("Success", {
+        description: "AI generated answer has been generated",
+      });
+    } catch (error) {
+      toast.error(`Error`, {
+        description: error instanceof Error ? error.message : "There was a problem with your request",
+      });
+    } finally {
+      setIsAISubmitting(false);
+    }
   };
 
   return (
@@ -59,6 +112,7 @@ const AnswerForm = ({ questionId }: { questionId: string }) => {
         <Button
           className="btn light-border-2 gap-1.5 rounded-md border px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
           disabled={isAISubmitting}
+          onClick={generateAIAnswer}
         >
           {isAISubmitting ? (
             <>
--- lib/validation.ts
+++ lib/validation.ts
@@ -142,4 +142,5 @@ export const AIAnswerSchema = z.object({
     .min(5, { message: "Question is required." })
     .max(130, { message: "Question cannot exceed 130 characters." }),
   content: z.string().min(100, { message: "Answer has to have more than 100 characters." }),
+  userAnswer: z.string().optional(),
 });
